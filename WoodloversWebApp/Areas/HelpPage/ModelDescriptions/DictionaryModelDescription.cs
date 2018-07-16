adbdd9a33778616f138732e6a832fb7b7925364d
edukng623 <edujorge13@gmail.com>
2018-07-17 00:55:37 +0100

    Add project files.

---
+++ WoodloversWebApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WoodloversWebApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WoodloversWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WoodloversWebApp.WebApiApplication" Language="C#" %>
